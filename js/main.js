e43c07fe64dfeb5d8eb63f9265647af8b9d24cad
DunneDev <dunn0203@algonquinlive.com>
2023-03-10 10:37:16 -0500

    working panelsnap

--- js/main.js
+++ js/main.js
@@ -8,4 +8,24 @@ document.addEventListener("DOMContentLoaded", function () {
         delay: 0,
         duration: 500,
     });
+
+    const sections = document.querySelectorAll("section");
+    const navLinks = document.querySelectorAll("nav a");
+
+    function navListenerFactory(section) {
+        return function (e) {
+            e.preventDefault();
+            snap.snapToPanel(section);
+        };
+    }
+
+    navLinks.forEach((link, i) => {
+        link.addEventListener("click", navListenerFactory(sections[i]));
+    });
+
+    snap.on("activatePanel", (section) => {
+        const index = Array.from(sections).indexOf(section);
+        navLinks.forEach((link) => link.classList.remove("selected"));
+        navLinks[index].classList.add("selected");
+    });
 });

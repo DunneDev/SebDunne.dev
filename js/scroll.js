1429a4fbef5e6fd5129f5453be864e8c00dd1b23
DunneDev <dunn0203@algonquinlive.com>
2023-03-16 13:58:24 -0400

    finished contact

--- js/scroll.js
+++ js/scroll.js
@@ -1,24 +1,24 @@
-const main = document.querySelector("main");
-const sectionHeight = window.innerHeight - convertRemToPixels(4.5);
+// const main = document.querySelector("main");
+// const sectionHeight = window.innerHeight - convertRemToPixels(4.5);
 
-toggleSelectedSection();
+// toggleSelectedSection();
 
-main.addEventListener("scroll", (e) => {
-    toggleSelectedSection();
-});
+// main.addEventListener("scroll", (e) => {
+//     toggleSelectedSection();
+// });
 
-function toggleSelectedSection() {
-    const sections = document.querySelectorAll("nav a");
-    sections.forEach((section, i) => {
-        if (i === Math.floor(main.scrollTop / sectionHeight + 0.5)) {
-            section.classList.add("selected");
-            console.log(section);
-        } else section.classList.remove("selected");
-    });
-}
+// function toggleSelectedSection() {
+//     const sections = document.querySelectorAll("nav a");
+//     sections.forEach((section, i) => {
+//         if (i === Math.floor(main.scrollTop / sectionHeight + 0.5)) {
+//             section.classList.add("selected");
+//             console.log(section);
+//         } else section.classList.remove("selected");
+//     });
+// }
 
-function convertRemToPixels(rem) {
-    return (
-        rem * parseFloat(getComputedStyle(document.documentElement).fontSize)
-    );
-}
+// function convertRemToPixels(rem) {
+//     return (
+//         rem * parseFloat(getComputedStyle(document.documentElement).fontSize)
+//     );
+// }
